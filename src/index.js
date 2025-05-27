f359ff1e8b62baded8fe4da61b47cbfc41d83cfe
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-27 16:39:28 +0300

    Added handler for calling dialog for adding new project.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-alert("Test");
+const addNewProjectButton = document.querySelector(".add-project-button");
+addNewProjectButton.addEventListener("click", () => {
+  const addNewProjectDialog = document.querySelector(".add-project-dialog");
+  addNewProjectDialog.showModal();
+});
