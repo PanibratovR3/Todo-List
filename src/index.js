696b27d1489ff587b10c36cdd373f94cea50e93c
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-27 17:24:20 +0300

    Added handler for submitting form for adding new project and printing it in console.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const addNewProjectButton = document.querySelector(".add-project-button");
+const addNewProjectDialog = document.querySelector(".add-project-dialog");
+const addNewProjectSubmitButton = document.querySelector(
+  ".add-project-submit-button"
+);
+
 addNewProjectButton.addEventListener("click", () => {
-  const addNewProjectDialog = document.querySelector(".add-project-dialog");
   addNewProjectDialog.showModal();
+});
+
+addNewProjectSubmitButton.addEventListener("click", () => {
+  const inputProjectName = document.querySelector("#project-name").value;
+  if (inputProjectName) {
+    console.log("Name of new project: " + inputProjectName);
+    const form = addNewProjectDialog.querySelector("form");
+    form.reset();
+    addNewProjectDialog.close();
+  }
 });
