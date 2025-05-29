005793a4cbae2924ab4341c0d4d334f91a9969d5
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-29 12:30:07 +0300

    Commented all rows for initial draw in html and index.js files.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import { Control } from "./control.js";
 import { Project } from "./project.js";
 import { DOMTodoList } from "./visualize.js";
-import { primaryStorage, downloadFromStorage } from "./storage.js";
 
-const projects = Control.getStorage();
-DOMTodoList.drawAllProjects(projects);
+// const projects = Control.getStorage();
+// DOMTodoList.drawAllProjects(projects);
 
-const addNewProjectButton = document.querySelector(".add-project-button");
-const addNewProjectDialog = document.querySelector(".add-project-dialog");
-const addNewProjectSubmitButton = document.querySelector(
-  ".add-project-submit-button"
-);
+// const addNewProjectButton = document.querySelector(".add-project-button");
+// const addNewProjectDialog = document.querySelector(".add-project-dialog");
+// const addNewProjectSubmitButton = document.querySelector(
+//   ".add-project-submit-button"
+// );
 
-addNewProjectButton.addEventListener("click", () => {
-  addNewProjectDialog.showModal();
-});
+// addNewProjectButton.addEventListener("click", () => {
+//   addNewProjectDialog.showModal();
+// });
 
-addNewProjectSubmitButton.addEventListener("click", () => {
-  const inputProjectName = document.querySelector("#project-name").value;
-  if (inputProjectName) {
-    const newProject = new Project(inputProjectName);
-    Control.addProjectToStorage(newProject);
-    const form = addNewProjectDialog.querySelector("form");
-    Control.addProjectToStorage;
-    form.reset();
-    addNewProjectDialog.close();
-    DOMTodoList.drawAllProjects(Control.getStorage());
-  }
-});
+// addNewProjectSubmitButton.addEventListener("click", () => {
+//   const inputProjectName = document.querySelector("#project-name").value;
+//   if (inputProjectName) {
+//     const newProject = new Project(inputProjectName);
+//     Control.addProjectToStorage(newProject);
+//     const form = addNewProjectDialog.querySelector("form");
+//     Control.addProjectToStorage;
+//     form.reset();
+//     addNewProjectDialog.close();
+//     DOMTodoList.drawAllProjects(Control.getStorage());
+//   }
+// });
